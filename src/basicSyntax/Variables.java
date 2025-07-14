0be66a818bc0196bd66e774756ea49dd8ffd10bd
Aoyagi Satoshi <aoyagi@example.com>
2025-07-14 18:11:55 +0900

    変数の使い方を学びました。

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,17 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int seisuNumber = 52;
+		double syousuNumber =1.2;	
+		String likeSeisu ="私の好きな整数は";
+		String likeSyousu ="私の好きな小数は";
+		System.out.println(seisuNumber);
+		System.out.println(syousuNumber);
+		System.out.println(likeSeisu+seisuNumber+"です。");
+		System.out.println(likeSyousu+syousuNumber+"です。");
+	}
+
+}
