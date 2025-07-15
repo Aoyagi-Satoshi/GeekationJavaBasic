f008f03aa1892a997b1072d2386e877438dbb5e2
Aoyagi Satoshi <aoyagi@example.com>
2025-07-15 12:36:19 +0900

    比較演算子と論理演算子の使い方を学びました。

---
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -0,0 +1,27 @@
+package basicSyntax;
+
+public class ComparisonLogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 15;
+		int b =10;
+		boolean isGreaterA = a>b;
+		System.out.println(isGreaterA);
+		
+		boolean isSunny = true;
+		boolean isWarm = true;
+		boolean weather = isSunny && isWarm;
+		System.out.println(weather);
+		
+		int x = 5;
+		int y =10;
+		boolean isGreaterOrEqualOrEvenNumber = x>=0&&y%2==0;
+		System.out.println(isGreaterOrEqualOrEvenNumber);
+		
+		boolean hasPermission = false;
+		 hasPermission = !(hasPermission);
+		 System.out.println(hasPermission);
+	}
+
+}
