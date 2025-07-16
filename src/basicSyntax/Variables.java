4cf1dc4fb807ed6b3b41e3ee3175e8fe3fa78f59
Aoyagi Satoshi <aoyagi@example.com>
2025-07-16 18:49:16 +0900

    コードのリファクタリングに関する変更を行いました。

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -4,14 +4,14 @@ public class Variables {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int seisuNumber = 52;
-		double syousuNumber = 1.2;
-		String likeSeisu = "私の好きな整数は";
-		String likeSyousu = "私の好きな小数は";
-		System.out.println(seisuNumber);
-		System.out.println(syousuNumber);
-		System.out.println(likeSeisu + seisuNumber + "です。");
-		System.out.println(likeSyousu + syousuNumber + "です。");
+		int integralNumber = 52;
+		double decimalNumber = 1.2;
+		String integral = "私の好きな整数は";
+		String decimal = "私の好きな小数は";
+		System.out.println(integralNumber);
+		System.out.println(decimalNumber);
+		System.out.println(integral + integralNumber + "です。");
+		System.out.println(decimal + decimalNumber + "です。");
 	}
 
 }
