0735e078b1eb90fcb6ca78e58eaa98fd8c36a393
Aoyagi Satoshi <aoyagi@example.com>
2025-07-16 13:26:46 +0900

    コードのリファクタリングとスタイルに関する変更を行いました。

--- src/basicSyntax/Calculation.java
+++ src/basicSyntax/Calculation.java
@@ -6,17 +6,17 @@ public class Calculation {
 		// TODO 自動生成されたメソッド・スタブ
 		int x = 5;
 		int y = 10;
-		int z = x+y;
-		System.out.println("変数zの値は"+z+"です");
-		
+		int z = x + y;
+		System.out.println("変数zの値は" + z + "です");
+
 		double a = 5.25;
-		a = a+2.5;
-		a = a*3;
-		System.out.println("変数aの値は"+a+"です");
-		int b = (int) (a);
-		b = b+10;
-		System.out.println("変数aの値は"+b+"です");
-		
+		a = a + 2.5;
+		a = a * 3;
+		System.out.println("変数aの値は" + a + "です");
+		int b = (int) a;
+		b = b + 10;
+		System.out.println("変数aの値は" + b + "です");
+
 	}
 
 }
