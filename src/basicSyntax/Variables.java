2545bb8fc78c2f266080b4a1563371e86671439c
Aoyagi Satoshi <aoyagi@example.com>
2025-07-16 14:04:59 +0900

    コードのリファクタリングとスタイルに関する変更を行いました。

--- src/basicSyntax/Variables.java
+++ src/basicSyntax/Variables.java
@@ -5,13 +5,13 @@ public class Variables {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int seisuNumber = 52;
-		double syousuNumber =1.2;	
-		String likeSeisu ="私の好きな整数は";
-		String likeSyousu ="私の好きな小数は";
+		double syousuNumber = 1.2;
+		String likeSeisu = "私の好きな整数は";
+		String likeSyousu = "私の好きな小数は";
 		System.out.println(seisuNumber);
 		System.out.println(syousuNumber);
-		System.out.println(likeSeisu+seisuNumber+"です。");
-		System.out.println(likeSyousu+syousuNumber+"です。");
+		System.out.println(likeSeisu + seisuNumber + "です。");
+		System.out.println(likeSyousu + syousuNumber + "です。");
 	}
 
 }
