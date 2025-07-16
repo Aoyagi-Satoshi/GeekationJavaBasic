8c5e217fc25d1a598e4cdb82fb15fd08613d8c89
Aoyagi Satoshi <aoyagi@example.com>
2025-07-16 14:08:14 +0900

    コードのリファクタリングとスタイルに関する変更を行いました。

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -5,23 +5,23 @@ public class ComparisonLogicalOperators {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int a = 15;
-		int b =10;
-		boolean isGreaterA = a>b;
+		int b = 10;
+		boolean isGreaterA = a > b;
 		System.out.println(isGreaterA);
-		
+
 		boolean isSunny = true;
 		boolean isWarm = true;
 		boolean weather = isSunny && isWarm;
 		System.out.println(weather);
-		
+
 		int x = 5;
-		int y =10;
-		boolean isGreaterOrEqualOrEvenNumber = x>=0&&y%2==0;
+		int y = 10;
+		boolean isGreaterOrEqualOrEvenNumber = x >= 0 && y % 2 == 0;
 		System.out.println(isGreaterOrEqualOrEvenNumber);
-		
+
 		boolean hasPermission = false;
-		 hasPermission = !(hasPermission);
-		 System.out.println(hasPermission);
+		hasPermission = !(hasPermission);
+		System.out.println(hasPermission);
 	}
 
 }
