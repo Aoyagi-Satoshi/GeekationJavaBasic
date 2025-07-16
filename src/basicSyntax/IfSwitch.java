aabf67f267e96850b92251b1bfaedf8b5da5499f
Aoyagi Satoshi <aoyagi@example.com>
2025-07-16 18:12:32 +0900

    コードのリファクタリングとスタイルに関する変更を行いました。

--- src/basicSyntax/IfSwitch.java
+++ src/basicSyntax/IfSwitch.java
@@ -5,29 +5,28 @@ public class IfSwitch {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int num = 6;
-		if(num%2==0){
+		if (num % 2 == 0) {
 			System.out.println("2の倍数です。");
-		}else if(num%3==0){
+		} else if (num % 3 == 0) {
 			System.out.println("3の倍数です。");
-		}else {
+		} else {
 			System.out.println("どちらでもありません。");
 		}
-	
 
 		String pref = "北海";
-		
+
 		switch (pref) {
-			case "北海":
-				System.out.println("北海道");
+		case "北海":
+			System.out.println("北海道");
 			break;
-			case "東京":
-				System.out.println("東京都");
+		case "東京":
+			System.out.println("東京都");
 			break;
-			case "京都":
-				System.out.println("京都府");
+		case "京都":
+			System.out.println("京都府");
 			break;
-			default:
-				System.out.println("当てはまりません。");
+		default:
+			System.out.println("当てはまりません。");
 			break;
 		}
 	}
