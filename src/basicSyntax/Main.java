9fefa462d54788bd9442492b0f3cf659c39799f2
Aoyagi Satoshi <aoyagi@example.com>
2025-07-17 14:52:06 +0900

    クラスについて学びました。

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+		int age = 27;
+		double height = 1.68;
 
+		GeekIntroduction.greeting(age, height);
+
+		int num1 = 1;
+		int num2 = 100;
+		GeekIntroduction.specialSkill(num1, num2);
 	}
 
 }
